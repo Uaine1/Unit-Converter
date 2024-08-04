4f7be497e9c73ff7f078743471f3706812bb5417
Uaine <angeloalcantara243@gmail.com>
2024-08-04 13:13:54 +0800

    Implement get_unit_list & show_results

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from PyQt5.QtWidgets import QMessageBox
 
 #def main():
 
@@ -51,6 +51,47 @@ def temp_converter(value, from_unit, to_unit):
         return (value - 273.15) * 9/5 + 32
     
     return value
+
+
+def get_unit_list(units_dropdown, from_dropdown, to_dropdown):
+    unit_type = units_dropdown.currentText()
+    from_dropdown.clear()
+    to_dropdown.clear()
+
+    if unit_type == 'Length':
+        units = ['meters', 'kilometers', 'miles', 'foot', 'centimeter', 'inch', 'decimeter', 'millimeter', \
+                 'micrometer', 'nautical mile', 'yard']
+        
+    elif unit_type == 'Weight':
+        units = ['grams', 'kilograms', 'pounds', 'ounce', 'ton']
+
+    elif unit_type == 'Temperature':
+        units = ['celsius', 'fahrenheit', 'kelvin']
+
+    from_dropdown.addItems(units)
+    to_dropdown.addItems(units)
+
+
+def show_results(value_input, from_dropdown, to_dropdown, units_dropdown, result_label, window):
+    try:
+        value = float(value_input.text())
+        from_unit = from_dropdown.currentText()
+        to_unit = to_dropdown.currentText()
+        unit_type = units_dropdown.currentText()
+        
+        if unit_type == 'Length':
+            conversion_result = length_converter(value, from_unit, to_unit)
+
+        elif unit_type == 'Weight':
+            conversion_result = weight_converter(value, from_unit, to_unit)
+
+        elif unit_type == 'Temperature':
+            conversion_result = temp_converter(value, from_unit, to_unit)
+            
+        result_label.setText(f'Result: {conversion_result}')
+        
+    except ValueError:
+        QMessageBox.warning(window, 'Invalid inputs', 'Please enter a number (Ex. 123)')
 
 
 if __name__ == "__main__":
